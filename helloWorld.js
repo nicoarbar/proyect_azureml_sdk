15ef59b107957ae0154f5f14abeefeb23dde94f6
Nicolas <48179607+nicoarbar@users.noreply.github.com>
2023-01-03 21:27:40 +0100

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -1,5 +1,12 @@
 //Ejemplo de codigo Javascirpt para mi pagina web
-var name = "Nicolas"
-var apellido = "Aragon"
+var name = "Nicolas";
+var apellido = "Aragon";
 
-alert("Hola " + name + apellido)
+alert("Hola " + name + apellido);
+
+
+var tweet = prompt("Compose your tweet");
+
+var nameLength = name.length;
+
+name.slice(0,1); // getting the first length of name
