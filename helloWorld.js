4e07e8fadda1619d5c54a494db635fb0511aaae8
Nicolas <nicolas.aragon@cognizant.com>
2023-01-05 10:59:44 +0100

    adding functions to js scripts

--- helloWorld.js
+++ helloWorld.js
@@ -1,12 +1,30 @@
 //Ejemplo de codigo Javascirpt para mi pagina web
-var name = "Nicolas";
+var my_name = "Nicolas";
 var apellido = "Aragon";
 
-alert("Hola " + name + apellido);
+alert("Hola " + my_name + apellido);
 
 
 var tweet = prompt("Compose your tweet");
 
-var nameLength = name.length;
+var my_nameLength = my_name.length;
 
-name.slice(0,1); // getting the first length of name
+my_name.slice(0,1); // getting the first charachter of my_name
+
+// getting the my_name from the prompt and capitalize the first letter
+var my_name = prompt('What is you my_name?');
+var first_char = my_name.slice(0,1);
+var first_char_upper = first_char.toUpperCase();
+var rest_of_name = my_name.slice(1, my_name.length);
+var capitalize_name = first_char_upper + rest_of_name
+alert('Hello ' + capitalize_name)
+
+
+//defining the function
+function getMilk{
+    alert('Going to get milk')
+    //console log for developers to debug the code
+    console.log('Going to get milk') 
+};
+// calling the function
+getMillk();
